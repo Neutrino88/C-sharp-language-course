1306b564e108b251ffecc2db619619c9b13e8bda
Oleg Romanov <romanov-97@mail.ru>
2017-07-26 01:09:42 +0300

    Create ComplexNumber struct; Add static methods Sum,Sub,Mul,Div for 2 arg; Override ToString()

---
+++ ComplexNumber/ComplexNumber.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CompNumber
+{
+    public struct ComplexNumber
+    {
+        public double rPart { get; private set; }
+        public double iPart { get; private set; }
+
+        public ComplexNumber(double realPart, double imagPart)
+        {
+            this.rPart = realPart;
+            this.iPart = imagPart;
+        }
+
+        public static ComplexNumber Sum(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.rPart + b.rPart, a.iPart + b.iPart);
+        }
+
+        public static ComplexNumber Sub(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.rPart - b.rPart, a.iPart - b.iPart);
+        }
+
+        public static ComplexNumber Mul(ComplexNumber a, ComplexNumber b)
+        {
+            double realPart = (a.rPart * b.rPart) - (a.iPart * b.iPart);
+            double imagPart = (a.rPart * b.iPart) + (a.iPart * b.rPart);
+
+            return new ComplexNumber(realPart, imagPart);
+        }
+
+        public static ComplexNumber Div(ComplexNumber a, ComplexNumber b)
+        {
+            double denominator = (Math.Pow(b.rPart, 2) + Math.Pow(b.iPart, 2));
+
+            double realPart = ((a.rPart * b.rPart) + (a.iPart * b.iPart)) / denominator;
+            double imagPart = ((a.rPart * b.iPart) - (a.iPart * b.rPart)) / denominator;
+
+            return new ComplexNumber(realPart, imagPart);
+        }
+
+        public override string ToString()
+        {
+            return $"{rPart} + {iPart}i";
+        }
+    }
+}
