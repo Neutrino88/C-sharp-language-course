aa0a4766f42f9600b17ecc4f5d48d4fbcc32c6ee
Oleg Romanov <romanov-97@mail.ru>
2017-08-03 19:46:06 +0300

    Add the possibility of parallel searching of the equations

--- BinarySearchRootEquation/BinarySearchRootEquation.cs
+++ BinarySearchRootEquation/BinarySearchRootEquation.cs
@@ -4,7 +4,7 @@
     {
         public delegate double Function(double x);
 
-        public static double FindRoot(Function func, double left, double right, double precision = 0.0001)
+        public static double FindRoot(double left, double right, double precision, Function func)
         {
             while (right - left > precision)
             {
@@ -25,6 +25,28 @@
             }
             
             return left;
+        }
+
+        public static double[] FindRoots(double left, double right, double precision, params Function[] funcs)
+        {
+            double[] results = new double[funcs.Length];
+            
+            System.Func<double, double, double, Function, double> solver = FindRoot;
+            System.IAsyncResult[] asyncResults = new System.IAsyncResult[funcs.Length];
+
+            int i = 0;
+            foreach (var func in funcs)
+            {
+                asyncResults[i++] = solver.BeginInvoke(left, right, precision, func, null, null);
+            }
+
+            i = 0;
+            foreach (var asyncResult in asyncResults)
+            {
+                results[i++] = solver.EndInvoke(asyncResult);
+            }
+
+            return results;
         }
     }
 }
