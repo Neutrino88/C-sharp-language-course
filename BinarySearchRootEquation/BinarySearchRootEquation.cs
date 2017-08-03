d66a963b9380a0c7688451087ab79fd4b0f7913e
Oleg Romanov <romanov-97@mail.ru>
2017-08-03 18:30:38 +0300

    Add binary search the root of Equation

---
+++ BinarySearchRootEquation/BinarySearchRootEquation.cs
@@ -0,0 +1,30 @@
+﻿namespace BinarySearchRootEquation
+{
+    public static class BinarySearchRootEquation
+    {
+        public delegate double Function(double x);
+
+        public static double FindRoot(Function func, double left, double right, double precision = 0.0001)
+        {
+            while (right - left > precision)
+            {
+                double midX = left + (right - left) / 2;
+                double midY = func.Invoke(midX);
+                double sign = midY * func.Invoke(left);
+
+                if (sign < 0)
+                {
+                    right = midX;
+                }else if (sign > 0)
+                {
+                    left = midX;
+                }else
+                {
+                    return (midY == 0 ? midX : left);
+                }
+            }
+            
+            return left;
+        }
+    }
+}
