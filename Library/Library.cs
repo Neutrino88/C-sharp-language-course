176d11f5776ec667de6660346c4a5c6385fb5acc
Oleg Romanov <romanov-97@mail.ru>
2017-08-30 20:40:30 +0300

    Modify GiveOutBook and ReturnBookToLibrary methods in Library class to invoke the exception if can't perform action

--- Library/Library.cs
+++ Library/Library.cs
@@ -1,118 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
-using hw.Library;
 using System.Linq;
 using System.Xml;
 
-[DataContract, KnownType(typeof(Book))]
-public class Library
+namespace hw.Library
 {
-    [DataMember] private LinkedList<Book> booksList;
-    public event EventHandler<BookAddedEventArgs> BookAdded;
-    public event EventHandler<CustomerAddedEventArgs> CustomerAdded;
-    public event EventHandler<BookStateChangedEventArgs> BookStateChanged;
-
-    public Library()
+    [DataContract, KnownType(typeof(Book))]
+    public class Library
     {
-        booksList = new LinkedList<Book>();
-    }
+        [DataMember] private LinkedList<Book> booksList;
+        public event EventHandler<BookAddedEventArgs> BookAdded;
+        public event EventHandler<CustomerAddedEventArgs> CustomerAdded;
+        public event EventHandler<BookStateChangedEventArgs> BookStateChanged;
 
-    public void AddBook(Book newBook)
-    {
-        this.booksList.AddLast(newBook);
-        this.BookAdded?.Invoke(this, new BookAddedEventArgs(newBook, $"A new book \"{newBook.Title}\" by {newBook.Author} was added to library"));
-    }
-
-    public IReadOnlyList<Book> GetAllBooks()
-    {
-        return this.booksList.ToList();
-    }
-
-    public IReadOnlyList<Book> GetFreeBooks()
-    {
-        return this.booksList.Where(book => book.Customer == null).ToList();
-    }
-
-    public IReadOnlyList<Book> GetTakenBooks()
-    {
-        return this.booksList.Where(book => book.Customer != null).ToList();
-    }
-
-    public IReadOnlyList<Book> FindBooksByAuthor(String author)
-    {
-        return this.booksList.Where(book => book.Author == author).ToList();
-    }
-
-    public IReadOnlyList<Book> FindBooksByTitle(String title)
-    {
-        return this.booksList.Where(book => book.Title == title).ToList();
-    }
-
-    public bool GiveOutBook(Book book, Customer customer)
-    {
-        if (customer.GetAllBooks().Count == 5 || 
-            customer.GetExpiredBooks().Count > 0 ||
-            customer.GetAllBooks().Count > 0 && customer.HasRarityBook && book.IsRarity)
+        public Library()
         {
-            return false;
+            booksList = new LinkedList<Book>();
         }
 
-        book.AddCustomer(customer);
-        customer.AddBook(book);
-
-        if (customer.GetAllBooks().Count == 1)
+        public void AddBook(Book newBook)
         {
-            this.CustomerAdded?.Invoke(this, new CustomerAddedEventArgs(customer, $"A new customer {customer.Name} with number {customer.Number} was added to library"));
+            this.booksList.AddLast(newBook);
+            this.BookAdded?.Invoke(this, new BookAddedEventArgs(newBook, $"A new book \"{newBook.Title}\" by {newBook.Author} was added to library"));
         }
 
-        this.BookStateChanged?.Invoke(this, new BookStateChangedEventArgs(book, BookState.IssuedToCustomer, $"A book \"{book.Title}\" by {book.Author} was issued to the customer", customer));
-
-        return true;
-    }
-
-    public void ReturnBookToLibrary(Book book)
-    {
-        book.Customer.DelBook(book);
-        book.DelCustomer();
-        this.BookStateChanged?.Invoke(this, new BookStateChangedEventArgs(book, BookState.ReturnedToLibrary, $"A book \"{book.Title}\" by {book.Author} was returned to the library"));
-
-    }
-
-    public void WriteToFile(String fileName)
-    {
-        DataContractSerializer ds = new DataContractSerializer(typeof(Library), null, 1000, false, true, null);
-        XmlWriterSettings setting = new XmlWriterSettings() { Indent = true };
-
-        using (XmlWriter writer = XmlWriter.Create(fileName, setting))
+        public IReadOnlyList<Book> GetAllBooks()
         {
-            ds.WriteObject(writer, this);
+            return this.booksList.ToList();
         }
-    }
 
-    public static Library ReadFromFile(String fileName)
-    {
-        DataContractSerializer ds = new DataContractSerializer(typeof(Library), null, 1000, false, true, null);
-
-        using (XmlReader reader = XmlReader.Create(fileName))
+        public IReadOnlyList<Book> GetFreeBooks()
         {
-            return (Library) ds.ReadObject(reader);
+            return this.booksList.Where(book => book.Customer == null).ToList();
         }
-    }
 
-    public Book this[String author, String title]
-    {
-        get
+        public IReadOnlyList<Book> GetTakenBooks()
         {
-            foreach (var book in this.booksList)
+            return this.booksList.Where(book => book.Customer != null).ToList();
+        }
+
+        public IReadOnlyList<Book> FindBooksByAuthor(String author)
+        {
+            return this.booksList.Where(book => book.Author == author).ToList();
+        }
+
+        public IReadOnlyList<Book> FindBooksByTitle(String title)
+        {
+            return this.booksList.Where(book => book.Title == title).ToList();
+        }
+
+        public void GiveOutBook(Book book, Customer customer)
+        {
+            if (book == null)     throw new ArgumentNullException("book");
+            if (customer == null) throw new ArgumentNullException("customer");
+            if (this.booksList.Find(book) == null) throw new ArgumentException("Book isn't found in library");
+            if (book.Customer != null) throw new InvalidOperationException("Book has been issued another customer");
+            if (customer.GetExpiredBooks().Count > 0)    throw new InvalidOperationException("Customer has an expired book");
+            if (customer.HasRarityBook && book.IsRarity) throw new InvalidOperationException("Customer already has a rarity book");
+            if (customer.GetAllBooks().Count == 5)       throw new InvalidOperationException("Customer has the maximum amount of books");
+            
+            book.AddCustomer(customer);
+            customer.AddBook(book);
+
+            if (customer.GetAllBooks().Count == 1)
             {
-                if (book.Author == author && book.Title == title)
-                {
-                    return book;
-                }
+                this.CustomerAdded?.Invoke(this, new CustomerAddedEventArgs(customer, $"A new customer {customer.Name} with number {customer.Number} was added to library"));
             }
 
-            return null;
+            this.BookStateChanged?.Invoke(this, new BookStateChangedEventArgs(book, BookState.IssuedToCustomer, $"A book \"{book.Title}\" by {book.Author} was issued to the customer", customer));
+        }
+
+        public void ReturnBookToLibrary(Book book)
+        {
+            if (book == null) throw new ArgumentNullException("book");
+            if (this.booksList.Find(book) == null) throw new ArgumentException("Book isn't belong this library");
+            if (this.booksList.Find(book).Value.Customer == null) throw new ArgumentException("Book is already in the library");
+            
+            book.Customer.DelBook(book);
+            book.DelCustomer();
+            this.BookStateChanged?.Invoke(this, new BookStateChangedEventArgs(book, BookState.ReturnedToLibrary, $"A book \"{book.Title}\" by {book.Author} was returned to the library"));
+        }
+
+        public void WriteToFile(String fileName)
+        {
+            DataContractSerializer ds = new DataContractSerializer(typeof(Library), null, 1000, false, true, null);
+            XmlWriterSettings setting = new XmlWriterSettings() { Indent = true };
+
+            using (XmlWriter writer = XmlWriter.Create(fileName, setting))
+            {
+                ds.WriteObject(writer, this);
+            }
+        }
+
+        public static Library ReadFromFile(String fileName)
+        {
+            DataContractSerializer ds = new DataContractSerializer(typeof(Library), null, 1000, false, true, null);
+
+            using (XmlReader reader = XmlReader.Create(fileName))
+            {
+                return (Library)ds.ReadObject(reader);
+            }
+        }
+
+        public Book this[String author, String title]
+        {
+            get
+            {
+                foreach (var book in this.booksList)
+                {
+                    if (book.Author == author && book.Title == title)
+                    {
+                        return book;
+                    }
+                }
+
+                return null;
+            }
         }
     }
- }
+}
